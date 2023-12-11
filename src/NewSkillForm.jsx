ec7d8864d5b7ac61af3d52fc8629411cd0cbd6f4
root <root@DESKTOP-PV5G1DG.>
2023-12-11 09:26:40 -0500

    Initial styling for part 2

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./NewSkillForm.css";
 
 export default function NewSkillForm({ addSkill }) {
     const [newSkill, setNewSkill] = useState("");
@@ -12,27 +13,26 @@ export default function NewSkillForm({ addSkill }) {
 
     return (
         <div>
-        <form onSubmit={handleAddSkill}>
-            <label>Skill</label>
-            <input 
-            value={newSkill}
-            type="text" 
-            onChange={(e) => setNewSkill(e.target.value)}
-            required
-            pattern=".{3,}"
-            />
-
-            <label>Level</label>
-            <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-
-            <button type="submit">ADD SKILL</button>
-        </form>
+            <form className="NewSkillForm" onSubmit={handleAddSkill}>
+                <label>Skill</label>
+                <input 
+                value={newSkill}
+                type="text" 
+                onChange={(e) => setNewSkill(e.target.value)}
+                required
+                pattern=".{3,}"
+                />
+                <label>Level</label>
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <br />
+                <button type="submit">ADD SKILL</button>
+            </form>
         </div>
     );
 }
